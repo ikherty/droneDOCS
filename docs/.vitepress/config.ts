0a9567adef8b60f3e274250ad82c4e3af87b1488
ikherty <ikherty@gmail.com>
2023-01-26 23:09:37 +0400

    update base in config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   title: "Drone Docs",
   description: "Справочник по FPV хобби",
   lang: "ru",
+  base: "/droneDOCS/",
   head: [["link", { rel: "icon", type: "image/x-icon", href: "/assets/img/favicon.png" }]],
   themeConfig: {
     siteTitle: "Drone Docs",
