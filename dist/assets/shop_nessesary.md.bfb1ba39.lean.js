d4f33ef941b5e5899c8f2e0f526d335a3f91b463
ikherty <ikherty@gmail.com>
2023-01-26 23:00:19 +0400

    remove dist folder

--- dist/assets/shop_nessesary.md.bfb1ba39.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,c as e,o as r,d as a}from"./app.270c6c84.js";const m=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"shop/nessesary.md"}'),l={name:"shop/nessesary.md"},n=a("",1),s=[n];function d(c,i,g,x,p,h){return r(),e("div",null,s)}const o=t(l,[["render",d]]);export{m as __pageData,o as default};
