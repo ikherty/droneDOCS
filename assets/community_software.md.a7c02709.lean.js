ec9d7b73d6f5e438da22fda4b4d2a43fc3b75c5a
ikherty <ikherty@users.noreply.github.com>
2023-06-04 10:58:32 +0000

    deploy: 9f32fb8263dc2a525c1350388e6d3da99124da2e

---
+++ assets/community_software.md.a7c02709.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as r,d as a}from"./app.00d2439c.js";const u=JSON.parse('{"title":"Используемое программное обеспечение","description":"","frontmatter":{},"headers":[],"relativePath":"community/software.md"}'),s={name:"community/software.md"},o=a("",18),l=[o];function i(n,p,h,g,c,b){return r(),t("div",null,l)}const m=e(s,[["render",i]]);export{u as __pageData,m as default};
